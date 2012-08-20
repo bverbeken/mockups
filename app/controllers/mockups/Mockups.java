32c47982e2cd0268f0cdaddc5608fc3b8309b046
Ben Verbeken <ben@ostia.be>
2012-08-20 17:07:42 +0200

    controller cleanup

--- app/controllers/mockups/Mockups.java
+++ app/controllers/mockups/Mockups.java
@@ -18,12 +18,12 @@ public class Mockups extends Controller {
     public static void show(String m) {
         if (isBlank(m)) {
             List<Mockup> mockups = allMockups();
-            render("mockups/Mockups/list.html", mockups);
+            render(mockups);
         } else {
             Mockup mockup = new Mockup(m);
             if (mockup.isDirectory()) {
                 List<Mockup> mockups = allMockups(m);
-                render("mockups/Mockups/list.html", mockups);
+                render(mockups);
             } else {
                 Template template = mockup.getTemplate();
                 HashMap<String, Object> args = new HashMap<String, Object>();
