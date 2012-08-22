462792f498333c469299a9ba0367928b7614ed75
Ben Verbeken <ben@ostia.be>
2012-08-22 08:14:57 +0200

    mockup is now rendered in temporary file

--- app/jobs/mockups/ThumbnailRenderer.java
+++ app/jobs/mockups/ThumbnailRenderer.java
@@ -3,8 +3,9 @@ package jobs.mockups;
 import play.exceptions.UnexpectedException;
 import play.jobs.Job;
 
-import java.io.*;
-import java.util.UUID;
+import java.io.File;
+
+import static java.io.File.createTempFile;
 
 public class ThumbnailRenderer extends Job<File> {
 
@@ -19,32 +20,13 @@ public class ThumbnailRenderer extends Job<File> {
         try {
             String phantomJsExecutable = "/usr/lib/phantomjs-1.6.1-linux-x86_64-dynamic/bin/phantomjs";
             String rasterizeScript = "/usr/lib/phantomjs-1.6.1-linux-x86_64-dynamic/examples/rasterize.js";
-            String targetFilename = UUID.randomUUID() + ".png";
-            String currentWorkingDir = "/home/bver";
-            String[] command = new String[]{phantomJsExecutable, rasterizeScript, url, targetFilename};
-            Process process = new ProcessBuilder(command).directory(new File(currentWorkingDir)).start();
-
-            redirectOutputToStdOut(process);
-
-            int result = process.waitFor();
-            if (result != 0) {
-                throw new RuntimeException("phantomjs exit code: " + result);
-            }
-
-            return new File(currentWorkingDir, targetFilename);
+            File tmpFile = createTempFile("mockup", ".png");
+            new ProcessBuilder(phantomJsExecutable, rasterizeScript, url, tmpFile.getAbsolutePath()).start().waitFor();
+            return tmpFile;
         } catch (Exception e) {
             play.Logger.error("Failed to generate thumbnail for url " + url, e);
             throw new UnexpectedException(e);
         }
     }
 
-    private static void redirectOutputToStdOut(Process process) throws IOException {
-        InputStream is = process.getInputStream();
-        InputStreamReader isr = new InputStreamReader(is);
-        BufferedReader br = new BufferedReader(isr);
-        String line;
-        while ((line = br.readLine()) != null) {
-            System.out.println(line);
-        }
-    }
 }
